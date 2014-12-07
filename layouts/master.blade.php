b79b34b793a450e12acbb07c12aa2293617ad43c
Nicolas Widart <n.widart@gmail.com>
2014-12-07 19:56:48 +0100

    Add the site title and description

--- layouts/master.blade.php
+++ layouts/master.blade.php
@@ -3,23 +3,21 @@
 <head lang="en">
     <meta charset="UTF-8">
     @section('meta')
-    <meta name="description" content="Nicolas Widart's Personal Portfolio. My name is Nicolas Widart, and I'm a web developper. I love the Laravel framework and new technologies." />
+        <meta name="description" content="{{ Setting::get('core::site-description') }}" />
     @show
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-    @section('title')
-       Site title
-    @show
-   </title>
-   <link rel="shortcut icon" href="{{ theme_url() }}/favicon.ico }}">
+        @section('title'){{ Setting::get('core::site-name') }}@show
+    </title>
+    <link rel="shortcut icon" href="{{ theme_url() }}/favicon.ico }}">
 
-   <?php if (App::environment() == 'development'): ?>
-       <link rel="stylesheet" href="{{ theme_url() }}/css/dist/bootstrap.min.css" media="screen">
-       <link rel="stylesheet" href="{{ theme_url() }}/css/prism.css">
-       <link rel="stylesheet" href="{{ theme_url() }}/css/bootswatch.css">
-   <?php else: ?>
-       <link rel="stylesheet" href="{{ theme_url() }}/css/dist/all.min.css">
-   <?php endif; ?>
+    <?php if (App::environment() == 'development'): ?>
+    <link rel="stylesheet" href="{{ theme_url() }}/css/dist/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" href="{{ theme_url() }}/css/prism.css">
+    <link rel="stylesheet" href="{{ theme_url() }}/css/bootswatch.css">
+    <?php else: ?>
+    <link rel="stylesheet" href="{{ theme_url() }}/css/dist/all.min.css">
+    <?php endif; ?>
 </head>
 <body>
 
@@ -31,11 +29,11 @@
 @include('partials.footer')
 
 <?php if (App::environment() == 'development'): ?>
-    <script src="{{ theme_url() }}/js/dist/jquery.min.js"></script>
-    <script src="{{ theme_url() }}/js/bootstrap.min.js"></script>
-    <script src="{{ theme_url() }}/js/prism.js"></script>
+<script src="{{ theme_url() }}/js/dist/jquery.min.js"></script>
+<script src="{{ theme_url() }}/js/bootstrap.min.js"></script>
+<script src="{{ theme_url() }}/js/prism.js"></script>
 <?php else: ?>
-    <script src="{{ theme_url() }}/js/dist/all.min.js"></script>
+<script src="{{ theme_url() }}/js/dist/all.min.js"></script>
 <?php endif; ?>
 @yield('scripts')
 </body>
