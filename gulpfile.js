77c922e3f701f063e177a86d9827014d333189fa
Nicolas Widart <n.widart@gmail.com>
2015-02-13 15:39:10 +0100

    Add stylist publish mixin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
+var gulp = require("gulp");
+var shell = require('gulp-shell');
 var elixir = require('laravel-elixir');
 
+elixir.extend("stylistPublish", function() {
+    gulp.task("stylistPublish", function() {
+        gulp.src("").pipe(shell("php ../../artisan stylist:publish"));
+    });
+    return this.queueTask("stylistPublish");
+});
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -18,7 +26,8 @@ elixir(function (mix) {
      */
     mix.less([
         "main.less"
-    ]);
+    ])
+    .stylistPublish();
 
     /**
      * Concat scripts
